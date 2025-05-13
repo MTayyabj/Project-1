3097fbf137f2d8cce3d9d95920e083b25861a25f
Muhammad Tayyab <tayyabj910189@gmail.com>
2025-05-13 21:38:37 +0500

    setting for versal

--- client/src/Components/SectionIntro/SectionIntro.jsx
+++ client/src/Components/SectionIntro/SectionIntro.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './SectionIntro.css';
- 
+import './Sectionintro.css';
 const SectionIntro = ({ heading, text }) => {
   return (
     <section className="section-intro">
       <div className="intro-container">
         <h2 className="intro-heading">{heading}</h2>
-        <p className="intro-text">{text}</p>
+        <p className="intro-text">{text}</p> 
       </div>
     </section>
   );
